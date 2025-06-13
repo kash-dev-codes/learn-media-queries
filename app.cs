558c1397c5e80c0b04f79fc8e4a0bb7341872d0d
Awaji Kachi <kash_dev_25@outlook.com>
2025-06-13 14:50:52 +0100

    ADD background and media query

---
+++ app.cs
@@ -0,0 +1,19 @@
+body{
+    background: blue;
+    color: white;
+}
+
+h1{
+    color:red;
+    font-size: 80px;
+    font-family: Georgia;
+}
+
+@media (max-width:500px){
+    body{
+        background: white;
+        color:blue;
+}
+
+
+
